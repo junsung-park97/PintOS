a23caabe63aeb67548bead24db32a4c893944140
junSung <not971006@gmail.com>
2025-10-01 16:14:40 +0900

    10,01/task/uninit타입->anon타입 초기화

--- pintos/include/vm/anon.h
+++ pintos/include/vm/anon.h
@@ -5,9 +5,11 @@ struct page;
 enum vm_type;
 
 struct anon_page {
+  // 미할당 상태 : SIZE_MAX or 할당 상태 : 해당 슬롯 idx
+  size_t slot_idx SIZE_MAX;
 };
 
-void vm_anon_init (void);
-bool anon_initializer (struct page *page, enum vm_type type, void *kva);
+void vm_anon_init(void);
+bool anon_initializer(struct page *page, enum vm_type type, void *kva);
 
 #endif
