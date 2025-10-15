9bd51976fe447cb3869409b907f41350b465e09a
anjun206 <malssungjiwon@gmail.com>
2025-10-15 16:29:41 +0900

    Update file.h

--- pintos/include/vm/file.h
+++ pintos/include/vm/file.h
@@ -8,13 +8,6 @@
 
 struct page;
 
-struct mmap_file {
-  struct file *file; /* file_reopen() 한 핸들 (매핑 단위로 1개) */
-  void *base;        /* 매핑 시작 유저 주소 (page-aligned) */
-  size_t length;     /* 요청 길이 (바이트, 마지막 페이지는 일부만 사용 가능) */
-  struct list_elem elem;
-};
-
 struct file_page {
   struct file *file;
   off_t offset;
